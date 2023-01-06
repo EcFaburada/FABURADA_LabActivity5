a5f8faf43c77d7492b3566de081f31cf3b07b89b
EcFaburada <emmanuelchristian.faburada@gmail.com>
2023-01-06 18:05:31 +0800

    Rename LeapYear components Add LeapYear main function Complete LeapYear Add exception handling to LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -1,8 +1,8 @@
 package LeapYear;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import  javax.swing.*;
+import  java.awt.event.ActionEvent;
+import  java.awt.event.ActionListener;
 
 public class  LeapYearGUI extends JFrame{
     private  JPanel panel1;
--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,8 @@
 package SimpleCalc;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import  javax.swing.*;
+import  java.awt.event.ActionEvent;
+import  java.awt.event.ActionListener;
 
 public class   SimpleCalcGUI extends JFrame {
     private JPanel panel1;
